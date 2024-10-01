a3118e10df6d66d220489b3ed4b216715ed47f02
jeffersonfparil <jeffersonparil@gmail.com>
2024-10-01 16:10:28 +1000

    reinitialising

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # epifeat
+
+Generate epistasis feature set for genomic prediction. Using an $n \times p$ matrix generate an nxs matrix of linear and non-linear combinations of the columns of the original matrix representing non-linear allele effects and high degrees of interactions within and across alleles and loci.
+
+|**Build Status**|**License**|
+|:--------------:|:---------:|
+| <a href="https://github.com/jeffersonfparil/epifeat/actions"><img src="https://github.com/jeffersonfparil/epifeat/actions/workflows/rust.yml/badge.svg"></a> | [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0) |
---
+++ src/io.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+
+pub fn slices(n: u64) !f64 {
+    var prng = std.rand.DefaultPrng.init(blk: {
+        var seed: u64 = undefined;
+        try std.posix.getrandom(std.mem.asBytes(&seed));
+        break :blk seed;
+    });
+    const rand = prng.random();
+    const allocator = std.heap.page_allocator;
+    var x = std.ArrayList(f64).init(allocator);
+    defer x.deinit();
+    var i: u64 = 0;
+    while (i < n) : (i += 1) {
+        try x.append(rand.float(f32));
+    }
+    for (x.items, 0..) |s, j| {
+        std.debug.print("x[{?}]={?}\n", .{ j, s });
+    }
+    const out: f64 = 0.0;
+    return out;
+}
+
+test "slices" {
+    const n: u64 = 10;
+    _ = try slices(n);
+}
